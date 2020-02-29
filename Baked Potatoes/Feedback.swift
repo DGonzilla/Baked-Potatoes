fab62033982b77c3fcb6566741254230b4f10469
David Adrien Gonzalez <visualsbygonzo@gmail.com>
2020-02-29 01:25:37 -0500

    Updates to Feedback View * Background image added * Text fields have borders * Slight changes in text

--- Baked Potatoes/Feedback.swift
+++ Baked Potatoes/Feedback.swift
@@ -9,9 +9,103 @@
 import SwiftUI
 
 struct Feedback: View {
+    
+    @State private var name: String = ""
+    @State private var email: String = ""
+    @State private var feedback: String = ""
+    
     var body: some View {
-        Text("Feedback")
-        .font(.largeTitle)
+        
+        ZStack {
+            
+            VStack{
+                
+                Image("Joker Movie Poster")
+                    .resizable()
+                    .edgesIgnoringSafeArea(.top)
+                    .opacity(0.57)
+            }
+            
+            VStack{
+                
+                Group{
+                    Text("Feedback")
+                        .font(.custom("Montserrat-Light", size: 60))
+                        .padding(.top, 70)
+                    
+                    Text("Tell us what we missed")
+                        .font(.custom("Montserrat-ExtraLightItalic", size: 20))
+                        .foregroundColor(.white)
+                        .padding(.top, 4)
+                }
+                
+                Group{
+                    
+                    Text("What's Your Name?")
+                        .padding(.top, 70)
+                        .font(.custom("Montserrat-Light", size: 20))
+                        .foregroundColor(.white)
+                    
+                    TextField("I rather be anonymous", text: $name)
+                        .multilineTextAlignment(.center)
+                        .font(.custom("Montserrat-Light", size: 18))
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .scaledToFit()
+                        .opacity(0.6)
+                    
+                    Image("MenuUnderline")
+                        .resizable()
+                        .frame(width: 300, height: 2)
+                        .cornerRadius(4.0)
+                    
+                    
+                    
+                    
+                    Text("Email")
+                        .padding(.top, 25)
+                        .font(.custom("Montserrat-Light", size: 20))
+                        .foregroundColor(.white)
+                    
+                    TextField("Promise we won't send junk 😉", text: $email)
+                        .multilineTextAlignment(.center)
+                        .font(.custom("Montserrat-Light", size: 18))
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .scaledToFit()
+                        .opacity(0.6)
+                    
+                    Image("MenuUnderline")
+                        .resizable()
+                        .frame(width: 300, height: 2)
+                        .cornerRadius(4.0)
+                    
+                    
+                    
+                    
+                    Text("Talk To Us")
+                        .padding(.top, 25)
+                        .font(.custom("Montserrat-Light", size: 20))
+                        .foregroundColor(.white)
+                    
+                    
+                    TextField("I would have really liked...", text: $feedback)
+                        .multilineTextAlignment(.center)
+                        .font(.custom("Montserrat-Light", size: 18))
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .scaledToFit()
+                        .opacity(0.6)
+                    
+                    
+                    Image("MenuUnderline")
+                        .resizable()
+                        .frame(width: 300, height: 2)
+                        .cornerRadius(4.0)
+                    
+                    
+                    
+                    Spacer()
+                }
+            }
+        }
     }
 }
 
